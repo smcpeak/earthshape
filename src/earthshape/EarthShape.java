73d1406e274902d64cba7c0eba8c0149a86b7782
Scott McPeak <scott.g.mcpeak@gmail.com>
2017-03-01 23:50:44 -0800

    Add some ability to layer text on top using a stack of sub-scenes.

--- src/earthshape/EarthShape.java
+++ src/earthshape/EarthShape.java
@@ -6,13 +6,18 @@ package earthshape;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.SubScene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
@@ -23,9 +28,13 @@ import javafx.stage.Stage;
 public class EarthShape extends Application {
     // Try holding a persistent Translate object so I can move
     // the camera.
-    private Translate cameraTranslate = new Translate(0, 0, -15);
+    private Translate cameraTranslate = new Translate(0, 0, -50);
 
-    public Group createSceneObjects() throws Exception {
+    /** Create and return a tree of objects to display. */
+    public Parent createSceneObjects() throws Exception {
+
+        // Group of objects to return.
+        Group root = new Group();
 
         // Texture?
         Image img = new Image("earthshape/textures/compass-rose.png");
@@ -34,41 +43,76 @@ public class EarthShape extends Application {
         PhongMaterial mat = new PhongMaterial();
         mat.setDiffuseMap(img);
 
-        // Box
+        // Box with texture applied.
         Box testBox = new Box(5, 5, 5);
         testBox.setMaterial(mat);
         //testBox.setMaterial(new PhongMaterial(Color.RED));
         //testBox.setDrawMode(DrawMode.LINE);
+        //testBox.setTranslateZ(3);
+        root.getChildren().add(testBox);
 
         // X axis
         PhongMaterial axisColor = new PhongMaterial(Color.RED);
         Box xaxis = new Box(100, 0.1, 0.1);
         xaxis.setMaterial(axisColor);
-
-        // Collect the objects together.
-        Group root = new Group();
         root.getChildren().add(xaxis);
-        root.getChildren().add(testBox);
+
+        // Tick marks along the X axis.
+        for (int i=-50; i < 50; i++) {
+            Box tick = new Box(0.1, 0.1, i%10==0? 3 : 1);
+            tick.setTranslateX(i);
+            tick.setMaterial(axisColor);
+            root.getChildren().add(tick);
+        }
+
         return root;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Create a scene containing the scene object graph.
-        Scene scene = new Scene(createSceneObjects(), 1024, 768,
-                                true /*depth buffer*/);
+        // Create a sub-scene containing the scene object graph.
+        SubScene scene = new SubScene(createSceneObjects(), 1024, 768,
+                                      true /*depth buffer*/,
+                                      SceneAntialiasing.BALANCED);
         scene.setFill(Color.ALICEBLUE);
 
         // Create and position camera
         PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setFarClip(1000);     // default is 100
         camera.getTransforms().addAll (
                 new Rotate(-20, Rotate.Y_AXIS),
                 new Rotate(-20, Rotate.X_AXIS),
                 this.cameraTranslate);
         scene.setCamera(camera);
 
+        // Make a stack pane to hold the sub-scene.
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(scene);
+
+        // Bind the sizes of the stack pane and sub-scene so when
+        // the stack resizes, the sub-scene does too.
+        scene.heightProperty().bind(stackPane.heightProperty());
+        scene.widthProperty().bind(stackPane.widthProperty());
+
+        // Add a group to hold labels that go above the 3D image.
+        Group labelGroup = new Group();
+        for (int i=0; i < 1000; i += 50) {
+            labelGroup.getChildren().add(new Text(i, i, "("+i+","+i+")"));
+        }
+
+        // Add another sub-scene to hold the labels.  This seems
+        // to be the way to get a useful coordinate system for
+        // placing the labels.
+        SubScene ss = new SubScene(labelGroup, 1024, 728);
+        stackPane.getChildren().add(ss);
+        ss.heightProperty().bind(stackPane.heightProperty());
+        ss.widthProperty().bind(stackPane.widthProperty());
+
+        // Wrap the stack pane in a full Scene.
+        Scene fullScene = new Scene(stackPane, 1024, 768);
+
         // Specify keyboard handling for the scene.
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        fullScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 // Very rudimentary WASD response.
@@ -96,8 +140,9 @@ public class EarthShape extends Application {
             }
         });
 
+
         primaryStage.setTitle("Earth Shape");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(fullScene);
         primaryStage.show();
     }
 
