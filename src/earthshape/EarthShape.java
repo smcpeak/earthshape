1c73c4cca7a38ed8b83e09f2828c42b557961980
Scott McPeak <scott.g.mcpeak@gmail.com>
2017-03-04 18:19:14 -0800

    Use perspective camera, add axes.

--- src/earthshape/EarthShape.java
+++ src/earthshape/EarthShape.java
@@ -5,6 +5,8 @@ package earthshape;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
@@ -16,6 +18,7 @@ import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.texture.Texture;
 import com.jogamp.opengl.util.texture.TextureData;
 import com.jogamp.opengl.util.texture.TextureIO;
@@ -23,7 +26,14 @@ import com.jogamp.opengl.util.texture.TextureIO;
 /** This application demonstrates a procedure for inferring the shape
   * of a surface (such as the Earth) based on the observed locations of
   * stars from various locations at a fixed point in time. */
-public class EarthShape extends Frame implements GLEventListener {
+public class EarthShape
+    // For now at least, continue to use plain AWT.
+    extends Frame
+    // Listen for GL draw events.
+    implements GLEventListener,
+               // Also handle keyboard input.
+               KeyListener
+{
     // AWT boilerplate.
     private static final long serialVersionUID = 3903955302894393226L;
 
@@ -43,6 +53,8 @@ public class EarthShape extends Frame implements GLEventListener {
                 EarthShape.this.dispose();
             }
         });
+
+        this.addKeyListener(this);
 
         this.setSize(800, 800);
         this.setLocationByPlatform(true);
@@ -111,11 +123,18 @@ public class EarthShape extends Frame implements GLEventListener {
         // Use a light blue background, full opacity.
         gl.glClearColor(0.8f, 0.9f, 1.0f, 1f);
 
-        // I'm not sure about these, but they are familiar from when
-        // I used OpenGL long ago.
+        // Specify camera projection.
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        gl.glOrtho(0, 1, 0, 1, -1, 1);
+
+        // Note: The combination of near clipping plane and the
+        // left/right/top/bottom values defines the field of view.
+        // If you move the clipping plane nearer the camera without
+        // adjusting the edges, the FOV becomes larger!
+        gl.glFrustum(-1, 1, -1, 1, 1, 100);
+
+        GLU glu = GLU.createGLU(gl);
+        glu.gluLookAt(1,1,2, 1,1,0, 0,1,0);
     }
 
     /** Release allocated resources associated with the GL context. */
@@ -133,7 +152,75 @@ public class EarthShape extends Frame implements GLEventListener {
         log("display");
         GL2 gl = (GL2)drawable.getGL();
 
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+        // Depth test for hidden surface removal, and normalize so
+        // I can freely scale my geometry without worrying about
+        // denormalizing the normal vectors.
+        gl.glEnable(GL.GL_DEPTH_TEST | GL2.GL_NORMALIZE);
+
+        // Draw a finite fragment of the X axis.
+        {
+            gl.glBegin(GL.GL_LINES);
+            gl.glColor3f(1,0,0);       // Red
+            gl.glVertex3f(100,0,0);    // +X at 100
+            gl.glVertex3f(-100,0,0);   // -X at 100
+
+            for (int i=-100; i < 100; i++) {
+                if (i == 0) { continue; }
+                float size = (i%10==0? 1.5f : 0.5f);
+                gl.glVertex3f(i, size, 0);
+                gl.glVertex3f(i, -size, 0);
+                gl.glVertex3f(i, 0, size);
+                gl.glVertex3f(i, 0, -size);
+            }
+
+            gl.glEnd();
+        }
+
+        // Draw a finite fragment of the Y axis.
+        {
+            gl.glBegin(GL.GL_LINES);
+            gl.glColor3f(0,0.5f,0);    // Dark green
+            gl.glVertex3f(0,100,0);    // +Y at 100
+            gl.glVertex3f(0,-100,0);   // -Y at 100
+
+            for (int i=-100; i < 100; i++) {
+                if (i == 0) { continue; }
+                float size = (i%10==0? 1.5f : 0.5f);
+                gl.glVertex3f(size, i, 0);
+                gl.glVertex3f(-size, i, 0);
+                gl.glVertex3f(0, i, size);
+                gl.glVertex3f(0, i, -size);
+            }
+
+            gl.glEnd();
+        }
+
+        // Draw a finite fragment of the Z axis.
+        {
+            gl.glBegin(GL.GL_LINES);
+            gl.glColor3f(0,0,1);       // Dark blue
+            gl.glVertex3f(0,0,100);    // +Z at 100
+            gl.glVertex3f(0,0,-100);   // -Z at 100
+
+            for (int i=-100; i < 100; i++) {
+                if (i == 0) { continue; }
+                float size = (i%10==0? 1.5f : 0.5f);
+                gl.glVertex3f(0, size, i);
+                gl.glVertex3f(0, -size, i);
+                gl.glVertex3f(size, 0, i);
+                gl.glVertex3f(-size, 0, i);
+            }
+
+            gl.glEnd();
+        }
+
+        // Scale everything that follows up by 2x.
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glPushMatrix();
+        gl.glScalef(2,2,2);
+        //gl.glScalef(0.5f,0.5f,0.5f);
 
         // Draw a colored triangle.
         {
@@ -181,6 +268,8 @@ public class EarthShape extends Frame implements GLEventListener {
             gl.glEnd();
         }
 
+        gl.glPopMatrix();
+
         gl.glFlush();
     }
 
@@ -196,4 +285,14 @@ public class EarthShape extends Frame implements GLEventListener {
     {
         (new EarthShape()).setVisible(true);
     }
+
+    @Override
+    public void keyPressed(KeyEvent ev) {
+        log("key pressed: "+ev);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ev) {}
+    @Override
+    public void keyTyped(KeyEvent ev) {}
 }
